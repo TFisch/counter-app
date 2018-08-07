3ab5e67343b21ce2d924dc4364ca82bf48bdd230
Tim Fischer <emailtimfischer@gmail.com>
2018-08-07 12:37:03 -0600

    Create classes var to change color based on color

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
     imageUrl: 'https://picsum.photos/200'
   };
+
   render() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+
     return (
       <React.Fragment>
         <img src={this.state.imageUrl} alt="image" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-small">Increment</button>
       </React.Fragment>
     );
   }

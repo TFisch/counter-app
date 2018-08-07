63e942c1b15862bb2c5cc4ee01ea769e6df84c37
Tim Fischer <emailtimfischer@gmail.com>
2018-08-07 12:39:38 -0600

    Refactor classes variable to method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1,
-    imageUrl: 'https://picsum.photos/200'
+    count: 1
   };
 
   render() {
-    let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    let classes = this.getBadgeClasses();
 
     return (
       <React.Fragment>
-        <img src={this.state.imageUrl} alt="image" />
         <span className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-small">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCount() {
